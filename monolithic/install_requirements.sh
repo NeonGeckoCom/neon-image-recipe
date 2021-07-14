112a209af3b3a9e06d44501c0573426ade425a0b
Daniel McKnight <daniel@neongecko.com>
2021-07-14 12:06:14 -0700

    Update install_requirements.sh script server deps Copy gui setup code to install_requirements.sh directly

--- monolithic/install_requirements.sh
+++ monolithic/install_requirements.sh
@@ -1,13 +1,22 @@
+
+if [ -z "${GITHUB_TOKEN}" ]; then
+  echo "GITHUB_TOKEN not specified!"
+  exit 1
+fi
+
 # update base image
 sudo apt-get update
 sudo apt-get upgrade
 
 # install system packages
 sudo apt-get install -y  alsa-utils \
-        libasound2 libasound2-plugins \
-        pulseaudio pulseaudio-utils \
-        sox libsox-fmt-all \
-        python3-pip network-manager swig libfann-dev gcc
+     libasound2 libasound2-plugins \
+     pulseaudio pulseaudio-utils \
+     sox libsox-fmt-all \
+     python3-pip network-manager swig libfann-dev gcc
+
+# Ubuntu Server Deps
+sudo apt install xorg openbox portaudio19-dev
 
 pip install --upgrade pip~=21.1
 pip install wheel
@@ -15,7 +24,67 @@ pip install "git+https://${GITHUB_TOKEN}@github.com/NeonDaniel/NeonCore@FEAT_PiS
 
 # mycroft-gui
 git clone https://github.com/mycroftai/mycroft-gui
-bash mycroft-gui/dev_setup.sh
+#bash mycroft-gui/dev_setup.sh
+cd mycroft-gui || exit 10
+TOP=$( pwd -L )
+sudo apt-get install -y git-core g++ cmake extra-cmake-modules gettext pkg-config qml-module-qtwebengine pkg-kde-tools \
+     qtbase5-dev qtdeclarative5-dev libkf5kio-dev libqt5websockets5-dev libkf5i18n-dev libkf5notifications-dev \
+     libkf5plasma-dev libqt5webview5-dev qtmultimedia5-dev
+
+echo "Building Mycroft GUI"
+if [[ ! -d build-testing ]] ; then
+  mkdir build-testing
+fi
+cd build-testing || exit 10
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release   -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+make -j4
+sudo make install
+
+echo "Installing Lottie-QML"
+cd "$TOP" || exit 10
+if [[ ! -d lottie-qml ]] ; then
+    git clone https://github.com/kbroulik/lottie-qml
+    cd lottie-qml || exit 10
+    mkdir build
+else
+    cd lottie-qml || exit 10
+    git pull
+fi
+
+cd build || exit 10
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release   -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+make
+sudo make install
+
+echo " "
+if [[ ! -f /etc/mycroft/mycroft.conf ]] ; then
+    if [[ ! -d /etc/mycroft ]] ; then
+        sudo mkdir /etc/mycroft
+    fi
+
+cat <<EOF | sudo tee /etc/mycroft/mycroft.conf
+{
+    "enclosure": {
+        "platform": "mycroft_mark_2"
+    }
+}
+EOF
+
+fi
+
+if [[ -f /etc/mycroft/mycroft.conf ]] ; then
+    echo "Found an existing Mycroft System Level Configuration at /etc/mycroft/mycroft.conf"
+    echo "Please add the following enclosure settings manually to existing configuration to ensure working setup:"
+    echo " "
+    echo '"enclosure": {'
+    echo '     "platform": "mycroft_mark_2"'
+    echo '}'
+    echo ""
+fi
+echo "Installation complete!"
+echo "To run, invoke:  mycroft-gui-app"
+exit 0
+
 rm -rf mycroft-gui
 sudo apt-get install libqt5multimedia5-plugins qml-module-qtmultimedia
 
