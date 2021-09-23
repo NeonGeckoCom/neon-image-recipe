7593d9841cd460f685034ab98437676c9849d2b0
Daniel McKnight <daniel@neongecko.com>
2021-09-23 14:46:06 -0700

    Relocate xvf3510-flash to sbin Update install and boot scripts (validated on 21.04)

--- base_mark_2/install_xmos_drivers.sh
+++ base_mark_2/install_xmos_drivers.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+kernel="5.4.0-1028-raspi"
+kernel="5.11.0-1007-raspi"
 # Install system dependencies
-sudo apt install -y gcc make python3-pip i2c-tools
-sudo pip install smbus smbus2 spidev rpi.gpio
+sudo apt install -y gcc make python3-pip i2c-tools libraspberrypi-bin
+sudo CFLAGS="-fcommon" pip install smbus smbus2 spidev rpi.gpio
 
 # Build and load VocalFusion Driver
 git clone https://github.com/OpenVoiceOS/vocalfusiondriver
 cd vocalfusiondriver/driver || exit 10
 make all
-sudo mkdir /lib/modules/5.4.0-1028-raspi/kernel/drivers/vocalfusion
-sudo cp vocalfusion* /lib/modules/5.4.0-1028-raspi/kernel/drivers/vocalfusion
+sudo mkdir /lib/modules/${kernel}/kernel/drivers/vocalfusion
+sudo cp vocalfusion* /lib/modules/${kernel}/kernel/drivers/vocalfusion
+# /usr/lib/modules/5.4.0-1028-raspi/extra/
 sudo depmod -a
 # modinfo vocalfusion-soundcard should show the module info now
 cd .. || exit 10
 sudo cp xvf3510.dtbo /boot/firmware/overlays
 
 # Copy required overlay files
-cd -P "$( dirname "${BASH_SOURCE[0]}" )" || exit 10
+cd .. || exit 10
 sudo cp -r overlay/* /
 sudo chmod -R ugo+x /usr/bin
+sudo chmod -R ugo+x /usr/sbin
 
 # Overwrite Pulse Config
 sudo rm /etc/pulse/system.pa
@@ -26,4 +30,3 @@ sudo rm /etc/pulse/daemon.conf
 sudo ln -s /etc/pulse/mycroft-sj201-daemon.conf /etc/pulse/daemon.conf
 sudo ln -s /etc/pulse/mycroft-sj201-default.pa /etc/pulse/system.pa
 
-echo "Add 'dtoverlay=xvf3510' line to /boot/firmware/config.txt"
