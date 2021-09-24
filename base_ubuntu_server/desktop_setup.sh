4b8aaf84269159c8bcfa84924bf09d3b87b12802
Daniel McKnight <daniel@neongecko.com>
2021-09-24 10:48:21 -0700

    Add desktop setup script with x config script

---
+++ base_ubuntu_server/desktop_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ubuntu Server Deps
+sudo apt update
+sudo apt install -y xorg openbox portaudio19-dev
+
+# Configure desktop settings
+sudo gsettings set org.gnome.desktop.screensaver lock-enabled false
+sudo gsettings set org.gnome.desktop.session idle-delay 0
+sudo sed -i "s/#  AutomaticLoginEnable = true/  AutomaticLoginEnable = true/" /etc/gdm3/custom.conf
+sudo sed -i "s/#  AutomaticLogin = user1/  AutomaticLogin = neon/" /etc/gdm3/custom.conf
+
+# setup X desktop environment
+sudo cp ./etc/profile.d/configure_x.sh /etc/profile.d/configure_x.sh
+chmod +x /etc/profile.d/configure_x.sh
+
+# Add 'neon' user
+sudo adduser neon --gecos "" --disabled-password
+echo "neon:neon" | sudo chpasswd
+sudo usermod -aG sudo neon
+echo "Added sudo user 'neon' remove Ubuntu user on next boot"
+
+echo "Verify Default DE after reboot"
+#echo -e "[SeatDefaults]\nautologin-user=neon" | sudo tee /etc/lightdm/lightdm.conf.d/50-myconfig.conf
