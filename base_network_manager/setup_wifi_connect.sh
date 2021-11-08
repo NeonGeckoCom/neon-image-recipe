1b6f0645037af410e8e1803179e520e3020b37ae
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-11-08 13:33:12 -0800

    Add wifi-connect overlay with setup script and updated README (#9)
    * Add wifi-connect overlay with setup script and updated README
    
    * Add wifi-connect ui to overlay files
    Update README with ToC
    
    * Update wifi setup to restart on failure

---
+++ base_network_manager/setup_wifi_connect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Replace netplan with network-manager
+sudo apt update
+sudo apt purge -y netplan.io
+sudo apt install -y network-manager
+
+# Setup all the services
+sudo systemctl stop systemd-networkd.socket
+sudo systemctl disable systemd-networkd.socket
+sudo systemctl stop systemd-networkd
+sudo systemctl disable systemd-networkd
+sudo systemctl enable network-manager
+
+# Add wifi-connect binary
+sudo cp -r overlay/* /
+sudo chmod -R ugo+x /usr/local/sbin
+sudo chmod -R ugo+x /opt/neon
+
+
+# Configure networking check on startup and restart
+sudo systemctl enable wifi-setup.service
+
+sudo shutdown -r
+history -c
