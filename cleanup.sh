f92ab12fddb996c9bf880914b2dbc1fcbe9fa856
Daniel McKnight <daniel@neongecko.com>
2021-11-19 16:09:38 -0800

    Set cleanup to enable fs resize

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 BASE_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${BASE_DIR}"/.. || exit 10
 sudo rm -r "${BASE_DIR}" || exit 2
-sudo rm /etc/NetworkManager/system-connections/* || exit 2
-# TODO: Enable resize partition on next boot DM
+sudo rm /etc/NetworkManager/system-connections/*
+sudo touch /opt/neon/resize_fs
 sudo shutdown
 history -c
