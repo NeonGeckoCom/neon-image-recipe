9e8ca4388b768ba86211d6d4d48237f3c486114c
Daniel McKnight <daniel@neon.ai>
2022-08-04 11:07:14 -0700

    Add validated embedded shell

--- 04_embedded_shell/install_gui_shell.sh
+++ 04_embedded_shell/install_gui_shell.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+ #!/bin/bash
 
 BASE_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${BASE_DIR}" || exit 10
 
 # Install gui base dependencies
-sudo apt-get install -y git-core g++ cmake extra-cmake-modules gettext pkg-config qml-module-qtwebengine pkg-kde-tools \
+apt update
+apt install -y git-core g++ cmake extra-cmake-modules gettext pkg-config qml-module-qtwebengine pkg-kde-tools \
      qtbase5-dev qtdeclarative5-dev libkf5kio-dev libqt5websockets5-dev libkf5i18n-dev libkf5notifications-dev \
      libkf5plasma-dev libqt5webview5-dev qtmultimedia5-dev gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
      gstreamer1.0-libav qml-module-qtwayland-compositor libqt5multimedia5-plugins qml-module-qtmultimedia plasma-pa xorg \
@@ -12,22 +13,24 @@ sudo apt-get install -y git-core g++ cmake extra-cmake-modules gettext pkg-confi
      qml-module-qt-labs-folderlistmodel qt5ct qml-module-qtquick-shapes qml-module-qtquick-particles2 \
      qml-module-qtquick-templates2 qml-module-qtquick-xmllistmodel qml-module-qtquick-localstorage \
      qml-module-qmltermwidget qml-module-qttest qml-module-qtlocation qml-module-qtpositioning \
-     qml-module-qtgraphicaleffects qml-module-qtqml-models2\
+     qml-module-qtgraphicaleffects qml-module-qtqml-models2 kirigami2-dev \
      --no-install-recommends
 
 # Install embedded-shell
-git clone https://github.com/OpenVoiceOS/mycroft-embedded-shell
+git clone https://github.com/OpenVoiceOS/ovos-shell
 
 # Add customized splash screen
-cp -f neon_splashscreen.png mycroft-embedded-shell/application/qml/background.png
+cp -f neon_splashscreen.png ovos-shell/application/qml/background.png
+cp -f neon_logo.svg ovos-shell/application/icons/ovos-egg.svg
+rm ovos-shell/application/icons/ovos-wave.svg
 
-cd mycroft-embedded-shell || exit 10
+cd ovos-shell || exit 10
 cmake .
 bash prefix.sh
-make mycroft-embedded-shell
-sudo make install mycroft-embedded-shell
+make ovos-shell
+make install ovos-shell || exit 10
 cd "${BASE_DIR}" || exit 10
-rm -rf mycroft-embedded-shell
+rm -rf ovos-shell
 
 # Install GUI
 git clone https://github.com/mycroftai/mycroft-gui
@@ -41,9 +44,9 @@ if [[ ! -d build-testing ]] ; then
   mkdir build-testing
 fi
 cd build-testing || exit 10
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release   -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 make -j4
-sudo make install
+make install
 
 echo "Installing Lottie-QML"
 cd "$TOP" || exit 10
@@ -57,17 +60,19 @@ else
 fi
 
 cd build || exit 10
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release   -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 make
-sudo make install
+make install
 
 cd "${BASE_DIR}" || exit 10
 rm -rf mycroft-gui
 
 # Copy overlay files and enable gui service
-sudo cp -r overlay/* /
-sudo chmod -R ugo+x /usr/bin
+cp -r overlay/* /
+chmod -R ugo+x /usr/bin
+
+systemctl enable neon-gui
 
 # Fix tmp dir permissions
-sudo mkdir -p /tmp/neon
-sudo chmod 777 /tmp/neon
+mkdir -p /tmp/neon
+chmod 777 /tmp/neon
