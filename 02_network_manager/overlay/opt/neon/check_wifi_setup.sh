2557365aa71aadeda3b20b0df86fb4fd016a3d9b
Daniel McKnight <daniel@neon.ai>
2022-08-12 13:48:51 -0700

    Update wifi setup service to handle startup connection delay

--- 02_network_manager/overlay/opt/neon/check_wifi_setup.sh
+++ 02_network_manager/overlay/opt/neon/check_wifi_setup.sh
@@ -3,5 +3,15 @@
 ip route | grep default
 
 if [ $? != 0 ]; then
-  wifi-connect  --portal-ssid Neon
+    if [ -n $(ls /etc/NetworkManager/system-connections/) ]; then
+        echo "Network Configured, waiting for reconnection"
+        sleep 15
+        ip route | grep default
+    else
+        echo "No Networks Configured"
+    fi
+fi
+if [ $? != 0 ]; then
+    echo "Network not connected, starting wifi-connect"
+    wifi-connect  --portal-ssid Neon
 fi
