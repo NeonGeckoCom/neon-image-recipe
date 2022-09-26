40a849b4b5ab9dead288b0bc5af96697d0244d73
Daniel McKnight <daniel@neon.ai>
2022-09-26 15:44:53 -0700

    Minor wifi-setup service and setup fixes

--- 02_network_manager/overlay/opt/neon/check_wifi_setup.sh
+++ 02_network_manager/overlay/opt/neon/check_wifi_setup.sh
@@ -6,10 +6,10 @@ if [ $? != 0 ]; then
     if [ -n "$(ls /etc/NetworkManager/system-connections/)" ]; then
         echo "Network Configured, waiting for reconnection"
         sleep 15
-        ip route | grep default
     else
         echo "No Networks Configured"
     fi
+    ip route | grep default
 fi
 if [ $? != 0 ]; then
     echo "Network not connected, starting wifi-connect"
--- 02_network_manager/setup_wifi_connect.sh
+++ 02_network_manager/setup_wifi_connect.sh
@@ -33,7 +33,7 @@ cd "${BASE_DIR}" || exit 10
 # Replace netplan with network-manager
 apt update
 apt purge -y netplan.io
-apt install -y network-manager
+apt install -y network-manager iproute2
 
 #dist=$(grep "^Distributor ID:" <<<"$(lsb_release -a)" | cut -d':' -f 2 | tr -d '[:space:]')
 #
@@ -48,7 +48,7 @@ rm -rf /etc/cloud
 # Setup all the services
 systemctl disable systemd-networkd.socket
 systemctl disable systemd-networkd
-systemctl disable wpa_supplicant.service
+#systemctl disable wpa_supplicant.service
 systemctl disable dnsmasq
 usermod -aG netdev neon
 
