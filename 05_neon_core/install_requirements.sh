119b4fd5d26c52d5df742d25759cb09e6a4d55e0
Daniel McKnight <daniel@neongecko.com>
2022-11-22 12:23:28 -0800

    Revert mimic apt install change

--- 05_neon_core/install_requirements.sh
+++ 05_neon_core/install_requirements.sh
@@ -39,12 +39,12 @@ if [ "${dist}" == 'Ubuntu' ]; then
 fi
 
 # install system packages
-#apt install -y curl
-#curl https://forslund.github.io/mycroft-desktop-repo/mycroft-desktop.gpg.key | apt-key add - 2> /dev/null && \
-#echo "deb http://forslund.github.io/mycroft-desktop-repo bionic main" | tee /etc/apt/sources.list.d/mycroft-desktop.list
+apt install -y curl
+curl https://forslund.github.io/mycroft-desktop-repo/mycroft-desktop.gpg.key | apt-key add - 2> /dev/null && \
+echo "deb http://forslund.github.io/mycroft-desktop-repo bionic main" | tee /etc/apt/sources.list.d/mycroft-desktop.list
 apt update
 apt install -y sox gcc libfann-dev swig libssl-dev portaudio19-dev git libpulse-dev \
-    espeak-ng g++ wireless-tools plasma-nm unzip ffmpeg make || exit 1
+    espeak-ng g++ wireless-tools plasma-nm unzip ffmpeg make mimic || exit 1
 
 # Cleanup apt caches
 rm -rf /var/cache/apt/archives/*
