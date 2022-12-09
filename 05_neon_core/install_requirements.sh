1d3eea05098c80d6367473ffdee47608f9d70d7a
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-12-09 08:41:49 -0800

    Update default STT fallback to vosk (#54)
    * Update default fallback STT to vosk
    
    * Remove local/remote STT dependencies to prep for Python3.10 Compat.
    Add deepspeech installation to requirements for compat.

--- 05_neon_core/install_requirements.sh
+++ 05_neon_core/install_requirements.sh
@@ -61,7 +61,8 @@ cp -rf overlay/* / || exit 2
 cd /home/neon || exit 2
 
 # Install core and skills
-pip install "git+https://github.com/neongeckocom/neoncore@${CORE_REF:-dev}#egg=neon_core[core_modules,skills_required,skills_essential,skills_default,skills_extended,pi,local,remote]" || exit 11
+pip install https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-cp37-cp37m-linux_aarch64.whl
+pip install "git+https://github.com/neongeckocom/neoncore@${CORE_REF:-dev}#egg=neon_core[core_modules,skills_required,skills_essential,skills_default,skills_extended,pi]" || exit 11
 echo "Core Installed"
 neon-install-default-skills && echo "Default git skills installed" || exit 2
 
