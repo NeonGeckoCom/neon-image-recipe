75cabfe81acf1886067dbe4a8271c14d85006477
Daniel McKnight <daniel@neongecko.com>
2022-12-16 11:13:13 -0800

    Add update service with default script and screen overlay

--- 10_updater/overlay/opt/neon/neon_os_2022_12.sh
+++ 10_updater/overlay/opt/neon/neon_os_2022_12.sh
@@ -27,68 +27,65 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Set to exit on error
-set -Ee
+BASE_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd /tmp || exit 10
-source vars.sh
+core_ref=${1:-dev}
+venv_path="/home/neon/venv"
+backup_path="/home/neon/.cache/neon/venv_backup"
+pip_spec="git+https://github.com/neongeckocom/neoncore@${core_ref}#egg=neon_core[core_modules,skills_required,skills_essential,skills_default,skills_extended,pi]"
+timestamp=$(date +"%Y-%m-%d_%H-%M")
+update_log_path="/home/neon/.local/state/neon/${timestamp}_update"
 
-dist=$(grep "^Distributor ID:" <<<"$(lsb_release -a)" | cut -d':' -f 2 | tr -d '[:space:]')
-
-
-print_opts() {
-    clear
-    echo ""
-    echo "------------------"
-    echo "Neon Image Creator"
-    echo "------------------"
-    echo "0. Exit"
-    echo "1. Core Configuration"
-    echo "2. Network Manager"
-    echo "3. SJ201 (Mark 2)"
-    echo "4. Embedded Shell + GUI"
-    echo "5. Neon Core"
-    echo "6. Dashboard"
-    echo "7. Camera"
-    echo "8. Splash Screen"
-    echo "10. Neon Updater"
+backup_venv() {
+  echo "Backing up venv"
+  cp -r "${venv_path}" "${backup_path}"
+  chown -R neon:neon /home/neon
+  echo "venv backup complete"
 }
 
-get_choice() {
-    read -p "Select Option " opt
-    case ${opt} in
-        0) exit 0;;
-        1) bash 01_core_configuration/configure_default_user.sh;;
-        2) bash 02_network_manager/setup_wifi_connect.sh;;
-        3) bash 03_sj201/setup_sj201.sh;;
-        4) bash 04_embedded_shell/install_gui_shell.sh;;
-        5) bash 05_neon_core/install_requirements.sh;;
-        6) bash 06_dashboard/install_ovos_dashboard.sh;;
-        7) bash 07_camera/configure_camera.sh;;
-        8) bash 08_splash_screen/configure_splash.sh;;
-        10) bash 10_updater/configure_updates.sh;;
-        *) ;;
-    esac
+remove_backup() {
+  echo "Removing venv backup"
+  rm -rf "${backup_path}"
 }
 
-if [ ${1} == "all" ]; then
-    bash 01_core_configuration/configure_default_user.sh
-    if [ "${dist}" == 'Ubuntu' ]; then
-        bash 02_network_manager/setup_wifi_connect.sh
-        bash 03_sj201/setup_sj201.sh
-    fi
-    bash 04_embedded_shell/install_gui_shell.sh
-    bash 05_neon_core/install_requirements.sh
-    bash 06_dashboard/install_ovos_dashboard.sh
-    if [ "${dist}" == 'Ubuntu' ]; then
-        bash 07_camera/configure_camera.sh
-        bash 08_splash_screen/configure_splash.sh
-    fi
-    bash 10_updater/configure_updates.sh
-    exit 0
-fi
+restore_backup() {
+  if [ -d "${backup_path}" ]; then
+    rm -rf "${venv_path}"
+    mv "${backup_path}" "${venv_path}"
+    chown -R neon:neon /home/neon
+  else
+    echo "No Backup to Restore!"
+  fi
+}
 
-while true; do
-    print_opts
-    get_choice
-done
+do_python_update() {
+  . "${venv_path}/bin/activate"
+  mkdir -p "${update_log_path}"
+  pip install --upgrade pip
+  pip install --report --upgrade "${pip_spec}" > "${update_log_path}/pip_report.json"
+  deactivate
+  chown -R neon:neon /home/neon
+}
+
+validate_module_load() {
+  . "${venv_path}/bin/activate"
+  status=$(python3 "${BASE_DIR}/check_neon_modules.py")
+  deactivate
+  if [ "${status}" == "0" ]; then
+    echo "Update success"
+    return 0
+  else
+    echo "Update failed!"
+    restore_backup
+    return 1
+  fi
+}
+
+remove_backup
+systemctl stop neon
+systemctl stop gui-shell
+backup_venv || exit 2
+do_python_update || echo "Update Failed"
+validate_module_load
+systemctl start gui-shell
+systemctl start neon
