ca27eafe9a3509537950d665af2114561f662e80
Daniel McKnight <daniel@neongecko.com>
2023-02-15 12:13:21 -0800

    Troubleshooting root partition sizing

--- automation/cleanup.sh
+++ automation/cleanup.sh
@@ -69,8 +69,9 @@ sudo umount mnt/run/systemd/resolve || exit 10
 # Make squashFS
 mksquashfs mnt neon.squashfs -noappend
 root_filesize=$(($(stat --printf="%s" neon.squashfs) / 1048576))  # 1024*1024=1048576 (B->MB)
-echo "Root FS=${root_filesize}MB"
-root_part_end=$((root_filesize + 68))  # 64M boot + 4M buffer
+echo "Root FS=${root_filesize}MiB"
+echo "Root FS=$((root_filesize * 1048576))B"
+root_part_end=$((root_filesize + 64))  # 64M boot
 sudo umount mnt || exit 10
 rm -r mnt
 
